12c981a56d778796ffedfef855ec9c45c518ba30
Ramiro Ambrosetti <Ramiro_ambrosetti@hotmail.com>
2020-10-29 20:22:02 -0300

    Arañas creadas, y optimizacion del codigo

--- searcher/trzpiders/trzpiders/pipelines.py
+++ searcher/trzpiders/trzpiders/pipelines.py
@@ -1,42 +1,4 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-import scrapy
-from scrapy import signals
-from scrapy.exporters import CsvItemExporter
-from scrapy.exceptions import DropItem
-from scrapy import Request
-import csv
-
 class TrzpidersPipeline:
-    def __init__(self):
-        self.files = {}
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        pipeline = cls()
-        crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
-        crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
-        return pipeline
-
-    def spider_opened(self, spider):
-        file = open('%s_items.csv' % spider.name, 'w+b')
-        self.files[spider] = file
-        self.exporter = CsvItemExporter(file)
-        self.exporter.fields_to_export = ['titulo', 'precio', 'categoria']
-        self.exporter.start_exporting()
-
-    def spider_closed(self, spider):
-        self.exporter.finish_exporting()
-        file = self.files.pop(spider)
-        file.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
         return item
--- searcher/trzpiders/trzpiders/spiders/gezatek.py
+++ searcher/trzpiders/trzpiders/spiders/gezatek.py
@@ -1,10 +1,51 @@
 import scrapy
+import os
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.exceptions import CloseSpider
+from scrapy.crawler import CrawlerProcess
+from searcher.trzpiders.trzpiders.items import TrzpidersItem
 
-
-class GezatekSpider(scrapy.Spider):
-    name = 'gezatek'
-    allowed_domains = ['www.gezatek.com.ar']
-    start_urls = ['http://www.gezatek.com.ar/']
-
-    def parse(self, response):
+class GezatekSpider(CrawlSpider):
+    """
+        Spider que recolecta datos de la pagina www.gezatek.com.ar
+    """
+    name = 'gezatek' # Nombre de la araña
+    custom_settings = {'FEEDS': {'gezatek.csv': {'format': 'csv'}}} # Forma de exportar los datos recolectados
+    allowed_domains = ['www.gezatek.com.ar'] # Dominio que manejamos, del cual no puede salir
+    # TODO: Hacer que reciba la palabra a buscar y la reemplace al final
+    start_urls = ['https://www.gezatek.com.ar/tienda/?busqueda=nvidia'] # URL donde extraemos los datos
+    # Reglas que debera respetar la spider
+    rules = {
+        # Entra en cada item (para extraer los datos) y luego vuelve a la pagina de extraccion
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//div[@class="w-box product "]')),
+             callback='parse_item', follow=False)
+    }
+    # En el caso de existir el archivo, lo elimina
+    try:
+        os.remove('gezatek.csv')
+    except OSError:
         pass
+
+    def parse_item(self, response):
+        """
+            Recolecta la informacion de cada item
+        """
+        item = TrzpidersItem()
+        item['title'] = str(response.css(
+            '.nombre h3::text').extract_first()).strip()
+        item['price'] = str(response.css(
+            '.precio_web h7::text').extract_first()).strip()
+        item['category'] = str(response.css(
+            '.col-md-5 h7::text').extract_first()).capitalize().strip()
+        item['link'] = str(response.url)
+        yield item
+
+    def turn_on_spider(self):
+        """
+            Enciende el spider
+        """
+        process = CrawlerProcess(
+            {'USER_AGENT': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'})
+        process.crawl(GezatekSpider)
+        process.start()

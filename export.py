1ac5df9ce586a08bb8f4de7adee02c97f59faee8
JaviTaba <javier.tabarovsky@gmail.com>
2020-10-30 05:22:40 -0300

    Sorter adaptado para los nuevos .csv Export adaptado para recibir la lista de Sorter

--- export.py
+++ export.py
@@ -11,39 +11,16 @@ from datetime import datetime
 class Export:
 
     # TODO: La informacion debe venir ordenada
-    def __init__(self, product_to_search, page_product_price):
-        self.__page_product_price = page_product_price
+    def __init__(self, product_to_search, orderded_products):
+        self.__ordered_products = orderded_products
         self.date = datetime.now()
         self.__file_name = str(product_to_search) + "-" + str(self.date.day) + "-" + \
             str(self.date.month) + "-" + str(self.date.year) + ".csv"
-        self.__header = ("Page", "Product", "Price")
-        # TODO: Agregar categoria, link de la publicacion, fecha-hora
-        self.__write_header()
-
-    def __write_header(self):
-        with open(self.__file_name, "w", newline='') as file:
-            writer = csv.DictWriter(
-                file, fieldnames=self.__header, delimiter=";")
-            writer.writeheader()
-
-    def __write(self, **data):
-        with open(self.__file_name, "a", newline='') as file:
-            writer = csv.DictWriter(
-                file, fieldnames=self.__header, delimiter=";")
-            writer.writerow(data)
+        #category,link,price,time,title
 
     def write(self):
-        """
-            Escribe en un csv con los datos recibidos
-
-        """
-        for page, product_price in self.__page_product_price.items():
-            for product in product_price:
-                self.__write(Page=str(page), Product=str(
-                    product[0]), Price=str(product[1]))
-
-
-if __name__ == "__main__":
-    dic = {"CompraGamer": [["Nvidia RTX 2080", "70000"], [
-        "Nvidia RTX 2080 TI", "90000"]], "Venex": [["Nvidia RTX 2080 TI", "95000"]]}
-    Export("Nvidia RTX 2080", dic).write()
+        file = open(self.__file_name, "w", newline='\n')
+        with file:
+            write = csv.writer(file)
+            for product in self.__ordered_products:
+                write.writerow(product)

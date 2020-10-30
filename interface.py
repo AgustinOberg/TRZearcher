aef0636b130317e84524d65bc19c7d2d47c3d634
Agustin Aguilera <agustin.aguilera424@gmail.com>
2020-10-29 22:12:07 -0300

    BugFix

--- interface.py
+++ interface.py
@@ -3,33 +3,37 @@ class Interface:
 
     def __init__(self):
         self.pages = ["Compra Gamer", "Gezatek", "Venex", "Full H4rd"]
-        self.pages_to_search = []
-        self.product_to_search = PagesReader().get_url()
+        self.active_pages = []
+        self.pages_to_search = PagesReader().get_url()
         self.search_type = 1
         self.__message_product_to_search()
         self.__message_pages_to_search()
         self.__message_search_type()
         self.__results()
 
+
+
     def __message_product_to_search(self):
-        self.product_to_search = input("***************************************************************"
-                                       + "\n"
-                                       + "\n                     CHOOGLE"
-                                       + "\n                       TRZ"
-                                       + "\n"
-                                       + "\n Somos un buscador/comparador de precios de productos GAMERS"
-                                       + "\n"
-                                       + "\n Introduzca el producto a buscar: ")
+        self.product = input("***************************************************************"
+                                     + "\n"
+                                     + "\n                     CHOOGLE"
+                                     + "\n                       TRZ"
+                                     + "\n"
+                                     + "\n Somos un buscador/comparador de precios de productos GAMERS"
+                                     + "\n"
+                                     + "\n Introduzca el producto a buscar: ")
 
     def __message_pages_to_search(self):
         print("\n***************************************************************"
               + "\n"
               + "\n Responder con un si en el caso de querer incluirla")
+        i = 0
         for page in self.pages:
             boolean = input(
                 "\n Quieres incluir en tu comparacion a " + str(page) + "? ")
             if (boolean.lower() == "si"):
-                self.pages_to_search.append(page)
+                self.active_pages.append(self.pages_to_search[i])
+                i+=1
 
     def __message_search_type(self):
         self.search_type = input("\n ¿ Que tipo de busqueda quieres realizar ?"
@@ -44,19 +48,19 @@ class Interface:
         print("\n***************************************************************"
               + "\n"
               + "\n Se realizara la una busqueda de " +
-              str(self.product_to_search)
+              str(self.pages_to_search)
               + "\n "
               + "\n Tipo: " + str(self.search_type)
               + "\n "
               + "\n En las siguientes paginas:")
-        for page in self.pages_to_search:
+        for page in self.active_pages:
             print("- " + str(page))
 
     def get_pages_to_search(self):
-        return self.pages_to_search
+        return self.active_pages
 
     def get_product_to_search(self):
-        return self.product_to_search
+        return self.pages_to_search
 
     def get_search_type(self):
         return self.search_type
---
+++ orchestrator.py
@@ -0,0 +1,4 @@
+
+class Orchestrator:
+    def __init__(self):
+        

6f90cf2d97b29a53323d775866835a3bbac6916b
Ramiro Ambrosetti <Ramiro_ambrosetti@hotmail.com>
2020-11-05 03:50:24 -0300

    Se modularizo la Interfaz

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,56 +1,78 @@
 from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 from Connector.connector import Connector
 
-# TODO: Modularizar y crear metodo para el main
 # TODO: Poner mensaje cargando
-# TODO: Validar opciones u opciones por default
 
-window = Tk()
-window.title("TRZ - TRZearcher")
-window.resizable(False, False)
+class Interface (tk.Frame):
 
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.window = master
 
-frame = Frame(window, bg="#cce7e8", width='650', height='500', bd=10, relief='ridge')
-frame.pack()
+    def execute_interface(self):
+        self.__configurate_window()
+        self.__configurate_frame()
+        self.__configurate_labels()
+        self.__configurate_entry()
+        self.__configurate_checkbutton()
+        self.__configurate_radiobutton()
+        self.__configurate_button()
 
-Label(frame, text='Desarrollado por TRZ', font=('Comic Sans MS', 7), fg="#cce7e8", bg="black").place(x=532, y=460)
-Label(frame, text='BIENVENIDO A TRZEARCHER', font=('Comic Sans MS', 20), bg="#cce7e8").place(x=100, y=30)
-Label(frame, text='Producto a buscar', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=100)
-Label(frame, text='Paginas en las cuales buscar:', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=150)
-Label(frame, text='Tipo de busqueda:', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=290)
+    def __configurate_window(self):
+        self.window.title("TRZ - TRZearcher")
+        self.window.resizable(False, False)
 
-product = StringVar()
-Entry(frame, justify='center', font=('Comic Sans MS', 10), textvariable=product).place(x=326, y=106)
-#TODO : Validar que no sea vacio, JOPTIONPANE
+    def __configurate_frame(self):
+        self.frame = Frame(self.window, bg="#cce7e8", width='650', height='500', bd=10, relief='ridge')
+        self.frame.pack()
 
-compra_gamer = IntVar()
-Checkbutton(frame, text='Compra Gamer', font=('Comic Sans MS', 12), variable=compra_gamer, onvalue=1, offvalue=0,
-            bg="#cce7e8").place(x=135, y=180)
-full_hard = IntVar()
-Checkbutton(frame, text='Full H4rd', font=('Comic Sans MS', 12), variable=full_hard, onvalue=1, offvalue=0,
-            bg="#cce7e8").place(x=135, y=210)
-gezatek = IntVar()
-Checkbutton(frame, text='Gezatek', font=('Comic Sans MS', 12), variable=gezatek, onvalue=1, offvalue=0,
-            bg="#cce7e8").place(x=135, y=240)
-venex = IntVar()
-Checkbutton(frame, text='Venex', font=('Comic Sans MS', 12), variable=venex, onvalue=1, offvalue=0,
-            bg="#cce7e8").place(x=295, y=180)
-overdrive = IntVar()
-Checkbutton(frame, text='Overdrive', font=('Comic Sans MS', 12), variable=overdrive, onvalue=1, offvalue=0,
-            bg="#cce7e8").place(x=295, y=210)
+    def __configurate_labels(self):
+        Label(self.frame, text='Desarrollado por TRZ', font=('Comic Sans MS', 7), fg="#cce7e8", bg="black").place(x=532, y=460)
+        Label(self.frame, text='BIENVENIDO A TRZEARCHER', font=('Comic Sans MS', 20), bg="#cce7e8").place(x=100, y=30)
+        Label(self.frame, text='Producto a buscar', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=100)
+        Label(self.frame, text='Paginas en las cuales buscar:', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=150)
+        Label(self.frame, text='Tipo de busqueda:', font=('Comic Sans MS', 15), bg="#cce7e8").place(x=135, y=290)
 
-option = IntVar()
-radio_button_1 = Radiobutton(frame, text='Publicación con la frase exacta', variable=option, value=1, font=('Comic Sans MS', 12),
-            bg="#cce7e8").place(x=135, y=320)
-radio_button_2 = Radiobutton(frame, text='Publicación que contenga todas las palabras', variable=option, value=2,
-            font=('Comic Sans MS', 12), bg="#cce7e8").place(x=135, y=350)
-radio_button_3 = Radiobutton(frame, text='Publicación que contenga algunas de las palabras', variable=option, value=3,
-            font=('Comic Sans MS', 12), bg="#cce7e8").place(x=135, y=380)
+    def __configurate_entry(self):
+        self.product = StringVar()
+        Entry(self.frame, justify='center', font=('Comic Sans MS', 10), textvariable=self.product).place(x=326, y=106)
 
-def __code_button():
-    Connector(product.get(),compra_gamer.get(), full_hard.get(), gezatek.get(), venex.get(), overdrive.get(), option.get())
-    window.destroy()
+    def __configurate_checkbutton(self):
+        self.compra_gamer = IntVar()
+        Checkbutton(self.frame, text='Compra Gamer', font=('Comic Sans MS', 12), variable=self.compra_gamer, onvalue=1, offvalue=0, bg="#cce7e8").place(x=135, y=180)
+        self.compra_gamer.set(1)
+        self.full_hard = IntVar()
+        Checkbutton(self.frame, text='Full H4rd', font=('Comic Sans MS', 12), variable=self.full_hard, onvalue=1, offvalue=0, bg="#cce7e8").place(x=135, y=210)
+        self.full_hard.set(1)
+        self.gezatek = IntVar()
+        Checkbutton(self.frame, text='Gezatek', font=('Comic Sans MS', 12), variable=self.gezatek, onvalue=1, offvalue=0, bg="#cce7e8").place(x=135, y=240)
+        self.gezatek.set(1)
+        self.venex = IntVar()
+        Checkbutton(self.frame, text='Venex', font=('Comic Sans MS', 12), variable=self.venex, onvalue=1, offvalue=0, bg="#cce7e8").place(x=295, y=180)
+        self.venex.set(1)
+        self.overdrive = IntVar()
+        Checkbutton(self.frame, text='Overdrive', font=('Comic Sans MS', 12), variable=self.overdrive, onvalue=1, offvalue=0, bg="#cce7e8").place(x=295, y=210)
+        self.overdrive.set(1)
 
-Button(frame, text="BUSCAR", font=('Comic Sans MS', 13), bg="dark gray", command=__code_button,
-       fg="#cce7e8", background='black').place(x=270, y=430)
-window.mainloop()
+    def __configurate_radiobutton(self):
+        self.option = IntVar()
+        Radiobutton(self.frame, text='Publicación con la frase exacta', variable=self.option, value=1, font=('Comic Sans MS', 12),
+                    bg="#cce7e8").place(x=135, y=320)
+        Radiobutton(self.frame, text='Publicación que contenga todas las palabras', variable=self.option, value=2,
+                    font=('Comic Sans MS', 12), bg="#cce7e8").place(x=135, y=350)
+        Radiobutton(self.frame, text='Publicación que contenga algunas de las palabras', variable=self.option, value=3,
+                    font=('Comic Sans MS', 12), bg="#cce7e8").place(x=135, y=380)
+        self.option.set(3)
+
+    def __code_button(self):
+        if (str(self.product.get()).strip() == "") and (self.compra_gamer.get() + self.full_hard.get() + self.gezatek.get() + self.venex.get() + self.overdrive.get() <= 0):
+            messagebox.showerror(message="Recordar que:\n - La entrada del producto no puede estar vacia\n - Al menos hay que seleccionar una pagina", title="Problema con TRZearcher")
+        else:
+            Connector(self.product.get(),self.compra_gamer.get(), self.full_hard.get(), self.gezatek.get(), self.venex.get(), self.overdrive.get(), self.option.get())
+            self.window.destroy()
+
+    def __configurate_button(self):
+        Button(self.frame, text="BUSCAR", font=('Comic Sans MS', 13), bg="dark gray", command=self.__code_button, fg="#cce7e8", background='black').place(x=270, y=430)
+
